e88999178622ba67627f26769ad3092ab488cf50
ridafkih <hello@rida.dev>
2026-01-06 04:19:52 -0500

    fix(database): do not import drizzle-kit in drizzle.config.ts

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -1,19 +1,13 @@
-import type { Config } from "drizzle-kit";
-import { defineConfig } from "drizzle-kit";
 import { join } from "node:path";
 
-export default ((): Config | void => {
-  if (process.env.DATABASE_URL) {
-    return defineConfig({
-      dbCredentials: {
-        url: process.env.DATABASE_URL,
-      },
-      dialect: "postgresql",
-      out: "./drizzle",
-      schema: [
-        join(__dirname, "src", "database", "schema.ts"),
-        join(__dirname, "src", "database", "auth-schema.ts"),
-      ],
-    });
-  }
-})();
+export default {
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+  dialect: "postgresql",
+  out: "./drizzle",
+  schema: [
+    join(__dirname, "src", "database", "schema.ts"),
+    join(__dirname, "src", "database", "auth-schema.ts"),
+  ],
+};
