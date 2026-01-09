0bc5149f3cb19f2b4376d36cff88db753fbf8b55
ridafkih <hello@rida.dev>
2026-01-09 01:17:40 -0500

    fixup! feat: wip: a calendar i can look at

--- applications/web/src/app/playground/(dashboard)/dashboard/page.tsx
+++ applications/web/src/app/playground/(dashboard)/dashboard/page.tsx
@@ -5,11 +5,10 @@ import { CalendarGrid } from "../../compositions/calendar-grid/calendar-grid";
 const DashboardPage = () => (
   <div className="flex flex-col gap-8 py-8">
     <div className="flex flex-col gap-4">
-      <Heading1>Home</Heading1>
-      <Copy>Welcome, Rida. It&apos;s Thursday the 8th and you&apos;ve got 4 events today across 14 calendars.</Copy>
+      <Copy>Welcome, Rida.</Copy>
+      <Copy>It&apos;s Friday the 9th and you&apos;ve got 5 events today across 2 calendars.</Copy>
     </div>
     <div className="flex flex-col gap-4">
-      <Heading2>Calendar</Heading2>
       <CalendarGrid />
     </div>
   </div>
