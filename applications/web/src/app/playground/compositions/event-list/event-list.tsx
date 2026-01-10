f300cd70475dae0ea795adb3a1fc730eabc0ce35
ridafkih <hello@rida.dev>
2026-01-10 15:17:53 -0500

    feat: wip: add calendar list and calendar list management pages, prototypes

--- applications/web/src/app/playground/compositions/event-list/event-list.tsx
+++ applications/web/src/app/playground/compositions/event-list/event-list.tsx
@@ -1,33 +1,21 @@
 "use client";
 
 import type { FC } from "react";
-import { useId, useState } from "react";
 import { EventItem } from "./components/event-item";
 import type { PlaygroundEvent } from "./utils/mock-events";
+import { List } from "../../components/list";
 
 interface EventListProps {
   events: PlaygroundEvent[];
 }
 
-const EventList: FC<EventListProps> = ({ events }) => {
-  const indicatorLayoutId = useId();
-  const [activeEventId, setActiveEventId] = useState<string | null>(null);
-
-  return (
-    <div className="flex flex-col">
-      {events.map((event) => (
-        <EventItem
-          key={event.id}
-          event={event}
-          isActive={activeEventId === event.id}
-          onMouseEnter={() => setActiveEventId(event.id)}
-          onMouseLeave={() => setActiveEventId(null)}
-          indicatorLayoutId={indicatorLayoutId}
-        />
-      ))}
-    </div>
-  );
-};
+const EventList: FC<EventListProps> = ({ events }) => (
+  <List>
+    {events.map((event) => (
+      <EventItem key={event.id} event={event} />
+    ))}
+  </List>
+);
 
 export { EventList };
 export type { EventListProps };
