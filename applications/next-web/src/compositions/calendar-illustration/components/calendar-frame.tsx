057b564a8fbbf5989a83e15973b0b22b1115102f
ridafkih <hello@rida.dev>
2026-01-21 23:09:33 -0500

    fixup! fix(next-web): calendar borders too high contrast

--- applications/next-web/src/compositions/calendar-illustration/components/calendar-frame.tsx
+++ applications/next-web/src/compositions/calendar-illustration/components/calendar-frame.tsx
@@ -1,5 +1,5 @@
 import type { FC, PropsWithChildren } from "react"
 
 export const CalendarFrame: FC<PropsWithChildren> = ({ children }) => {
-  return <div className="p-0.5 bg-surface-subtle">{children}</div>
+  return <div className="p-0.5 bg-border">{children}</div>
 }
